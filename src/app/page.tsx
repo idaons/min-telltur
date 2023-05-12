e61a70eeeb8759a6d941c57dc99e8174cb9c23cb
Daniel Winsvold <danielwinsvold@gmail.com>
2023-05-12 23:20:45 +0200

    heading og attribution til ida

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,18 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <MapWithNoSSR />
+      <div className={styles.info}>
+        <h1>
+          Turmål,{" "}
+          <a href="https://www.telltur.no/friluftsrad/salten">
+            Ti på topp Hamarøy
+          </a>
+        </h1>
+        <p>
+          Utviklet av{" "}
+          <a href="https://github.com/idaons/min-telltur">Ida Onshus</a>
+        </p>
+      </div>
     </main>
   );
 }
