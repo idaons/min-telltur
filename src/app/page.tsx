b2c9011dea7bd290eed870cc26e03ea0446cc932
Daniel Winsvold <danielwinsvold@gmail.com>
2023-05-17 00:08:43 +0200

    info-tekst i modal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,22 @@
 "use client";
-import { Box, Center, Heading, Link, Spinner } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Center,
+  Heading,
+  Link,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Spinner,
+  Stack,
+  Text,
+  useDisclosure,
+} from "@chakra-ui/react";
 import dynamic from "next/dynamic";
 
 export default function Home() {
@@ -37,8 +54,60 @@ export default function Home() {
             Ti på topp Hamarøy
           </Link>
         </Heading>
-        <Link href="https://github.com/idaons/min-telltur">Github</Link>
+        <Info />
       </Box>
+    </Box>
+  );
+}
+
+function Info() {
+  const { isOpen, onOpen, onClose } = useDisclosure({ defaultIsOpen: true });
+
+  return (
+    <Box>
+      <Link as="button" onClick={onOpen}>
+        Info
+      </Link>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent marginX="1rem">
+          <ModalHeader>Turmål, Ti på topp Hamarøy</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody paddingBottom="2rem">
+            <Stack spacing="1rem">
+              <Text>
+                Dette er et lite hobbyprosjekt jeg lagde fordi jeg ønsket meg en
+                enkel oversikt over turmål i Ti på topp Hamarøy.
+              </Text>
+              <Text>
+                Det er ingen automatisk kobling mellom dette kartet og
+                Telltur.no, så ting du gjør her gir deg ikke poeng eller
+                synkroniseres noe sted.
+              </Text>
+              <Text>
+                Jeg har gjort det mulig å huke av for {'"Besøkt"'}, men dette er
+                bare så det skal være lett å få oversikt og er ikke koblet til
+                telltur. Når du huker av for {'"Besøkt"'} her lagres det lokalt
+                i nettleseren din, og kan forsvinne plutselig.
+              </Text>
+              <Text>
+                Hvis du oppdager feil må du gjerne si ifra, så skal jeg rette
+                opp så fort som mulig.
+              </Text>
+              <Text>
+                Håper dette lille verktøyet er nyttig i turplanleggingen din.
+                God tur!
+              </Text>
+              <Text>
+                <Link href="https://github.com/idaons/min-telltur">
+                  Hvis du ønsker å bidra ligger koden på Github.
+                </Link>
+              </Text>
+            </Stack>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }
