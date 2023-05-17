24e9aa26c4f0d8848abecc11f8efb5cf4fefdc96
Ida Onshus <25682301+idaons@users.noreply.github.com>
2023-05-17 08:11:35 +0200

    textendring

--- src/app/Info.tsx
+++ src/app/Info.tsx
@@ -30,27 +30,25 @@ export function Info() {
           <ModalBody paddingBottom="2rem">
             <Stack spacing="1rem">
               <Text>
-                Dette er et lite hobbyprosjekt jeg lagde fordi jeg ønsket meg en
-                enkel oversikt over turmål i Ti på topp Hamarøy.
+                Denne siden ble laget fordi jeg ønsket meg en oversikt over
+                plasseringene av turmålene i Ti på topp Hamarøy.
               </Text>
               <Text>
-                Det er ingen automatisk kobling mellom dette kartet og
-                Telltur.no, så ting du gjør her gir deg ikke poeng eller
-                synkroniseres noe sted.
+                Det er ingen kobling mellom dette kartet og Telltur.no, så ting
+                du gjør her gir deg ikke poeng eller synkroniseres noe sted.
               </Text>
               <Text>
                 Jeg har gjort det mulig å huke av for {'"Besøkt"'}, men dette er
-                bare så det skal være lett å få oversikt og er ikke koblet til
-                telltur. Når du huker av for {'"Besøkt"'} her lagres det lokalt
-                i nettleseren din, og kan forsvinne plutselig.
+                bare så det skal være lett å få oversikt over egne besøk og er
+                ikke koblet til telltur. Når du huker av for {'"Besøkt"'} her
+                lagres det lokalt i nettleseren din, og kan forsvinne plutselig.
               </Text>
               <Text>
                 Hvis du oppdager feil må du gjerne si ifra, så skal jeg rette
                 opp så fort som mulig.
               </Text>
               <Text>
-                Håper dette lille verktøyet er nyttig i turplanleggingen din.
-                God tur!
+                Håper dette kan være nyttig i turplanleggingen din. God tur!
               </Text>
               <Text>
                 <Link href="https://github.com/idaons/min-telltur">
